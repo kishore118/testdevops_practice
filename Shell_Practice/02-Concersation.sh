f8b8ec12eb44d26d74f45f6a8479cfc93d17e66b
Kishore_Devops <kishorelanka11@gmail.com>
2026-01-28 23:20:57 +0530

    actions

--- Shell_Practice/02-Concersation.sh
+++ Shell_Practice/02-Concersation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "Suresh:: Hello Ramesh, How are you?"
 echo "Ramesh:: Hi Suresh, Iam fine thanks for asking. How are you doing?"
 echo "Suresh:: doing fine. what are you learning Ramesh?"
---
+++ Shell_Practice/03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Suresh # no space between values and =
+PERSON2=Ramesh
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, Iam fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. what are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell, WHat about you?"
